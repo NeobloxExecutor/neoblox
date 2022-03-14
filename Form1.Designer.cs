a5e433343c32893153e9e867ffeccf19b4c8a9eb
Plextora <plextoraa.a@gmail.com>
2022-03-14 12:38:34 -0400

    Change some names

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace neoblox
 {
-    partial class Form1
+    partial class neoblox
     {
         /// <summary>
         /// Required designer variable.
@@ -29,10 +29,17 @@ namespace neoblox
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // neoblox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "neoblox";
+            this.Text = "Neoblox";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- Form1.cs
+++ Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace neoblox
 {
-    public partial class Form1 : Form
+    public partial class neoblox : Form
     {
-        public Form1()
+        public neoblox()
         {
             InitializeComponent();
         }
