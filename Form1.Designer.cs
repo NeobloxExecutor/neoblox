5b449ab89865793ce88b13bb791f85b67ca3f2be
Plextora <plextoraa.a@gmail.com>
2022-03-14 12:39:36 -0400

    Changed background color to black and forecolor to "seashell"

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -35,7 +35,9 @@ namespace neoblox
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ForeColor = System.Drawing.Color.SeaShell;
             this.Name = "neoblox";
             this.Text = "Neoblox";
             this.ResumeLayout(false);
