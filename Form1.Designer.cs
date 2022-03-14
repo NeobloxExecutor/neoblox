20a33d9c7c44659d4b55222b8ac2863ba3802a74
Plextora <plextoraa.a@gmail.com>
2022-03-14 14:24:18 -0400

    Changed background color (again lol)

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -58,7 +58,7 @@ namespace neoblox
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(16)))), ((int)(((byte)(16)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.panel1);
             this.ForeColor = System.Drawing.Color.SeaShell;
