7b79320f0c8f89ece71377ea62a9d01d6d674f15
Plextora <plextoraa.a@gmail.com>
2022-03-14 12:51:17 -0400

    Added title to black toolbar

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,15 +30,28 @@ namespace neoblox
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.panel1.Controls.Add(this.titleLabel);
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(801, 49);
             this.panel1.TabIndex = 0;
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Font = new System.Drawing.Font("Pixter-Granular Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.Location = new System.Drawing.Point(4, 5);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(106, 36);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "Neoblox";
             // 
             // neoblox
             // 
@@ -50,6 +63,8 @@ namespace neoblox
             this.ForeColor = System.Drawing.Color.SeaShell;
             this.Name = "neoblox";
             this.Text = "Neoblox";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -57,6 +72,7 @@ namespace neoblox
         #endregion
 
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label titleLabel;
     }
 }
 
