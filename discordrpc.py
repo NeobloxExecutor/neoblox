3517221a6735a6cce744fdb53fe8ce5510b36b1b
Plextora <plextoraa.a@gmail.com>
2022-03-20 15:06:29 -0400

    Modify DiscordRPC for Neoblox

--- discordrpc.py
+++ discordrpc.py
@@ -4,9 +4,11 @@ import time
 client_id = "953316205001842718"
 RPC = Presence(client_id)
 
+discordTime = time.mktime(time.localtime())
+
 RPC.connect()
 
-RPC.update(state="Neoblox is made by Plextora#0033", details="Exploiting in Roblox using Neoblox", large_image="neoblox-icon")
+RPC.update(details="Exploiting in Roblox using Neoblox", large_image="neoblox-icon", large_text="Made by Plextora#0033", small_image="roblox", small_text="ROBLOX", start=discordTime, buttons=[{"label": "Get Neoblox", "url": "https://github.com/Plextora/neoblox/releases"}])
 
 while True:
     time.sleep(3)
