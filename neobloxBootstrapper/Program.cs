f133570a7477b9a73f762dab5d3ed7025e8b81de
Plextora <plextoraa.a@gmail.com>
2022-05-01 19:22:27 -0400

    Completed Neoblox bootstrapper

--- neobloxBootstrapper/Program.cs
+++ neobloxBootstrapper/Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
+using System.Net;
+using System.IO;
+using System.IO.Compression;
 
 namespace neobloxBootstrapper
 {
@@ -10,7 +14,139 @@ namespace neobloxBootstrapper
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(@"@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@(//(((((&@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@(///((((%&@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@...........@@@@@@@@@@@@@@@@@@@@@@@@@@@@@((//(((((&@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@,............&@@@@@@@@@@@@@@@@@@@@@@@@@@#(//(((((&@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@*,............,%@@@@@@@@@@@@@@@@@@@@@@@@#(/((((((&@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@/,............,,,%@@@@@@@@@@@@@@@@@@@@@@@(/((((((&&@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@,............,,,,,%@@@@@@@@@@@@@@@@@@@@@(((((((((&@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@,...........,,,,,,,,(@@@@@@@@@@@@@@@@@@@(((((((((&@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@*,.......%@/,,,,,,,,,,*@@@@@@@@@@@@@@@@@#((((((((&@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@*,.......%@@@(*,,,,,,,***@@@@@@@@@@@@@@@@((((((((&@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@#,.......%@@@@@@*,,,,,*****&@@@@@@@@@@@@@((((((((&&@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@,.......(&@@@@@@@/,,,*******%@@@@@@@@@@@#((((((((&@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@*........%@@@@@@@@@/,*********&@@@@@@@@@#((((((((&@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@*,.......%@@@@@@@@@@@/********//#@@@@@@@##(((((((&@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@**.......%@@@@@@@@@@@@@(******/////@@@@@@#(((((((&&@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@*.......%%@@@@@@@@@@@@@@#/***///////@@@@#((((((((&@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@*........%@@@@@@@@@@@@@@@@@///////////@@#((((((((&@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@*,.......%@@@@@@@@@@@@@@@@@@@(/////////((((((((((&@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@*,.......%@@@@@@@@@@@@@@@@@@@@@(///////((((((((((&@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@/*.......%@@@@@@@@@@@@@@@@@@@@@@@(////(((((((((((&&@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@*.......%%@@@@@@@@@@@@@@@@@@@@@@@@#(/((((((((((((&@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@*.......,%@@@@@@@@@@@@@@@@@@@@@@@@@@%((((((((((((&@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@*,......,%@@@@@@@@@@@@@@@@@@@@@@@@@@@@@((((((((((&@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@/*.....,,%@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@*....,,%%@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
 
+            Thread.Sleep(2000);
+
+            // Console managment
+
+            void greenConsole()
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+            }
+
+            void redConsole()
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+            }
+
+            void normalConsole()
+            {
+                Console.ForegroundColor = ConsoleColor.Black;
+            }
+
+            void space()
+            {
+                Console.WriteLine("");
+            }
+
+            using (var client = new WebClient())
+            {
+                try
+                {
+                    space();
+
+                    greenConsole();
+                    Console.WriteLine("Downloading Neoblox...");
+                    space();
+                    normalConsole();
+
+                    if (File.Exists("Neoblox.zip") != false)
+                    {
+                        redConsole();
+                        Console.WriteLine("Neoblox.zip is already downloaded in this directory!");
+                        space();
+                        normalConsole();
+                    }
+
+                    if (File.Exists("Neoblox.zip") == false)
+                    {
+                        greenConsole();
+                        client.DownloadFile("https://plextora.is-from.space/r/Neoblox.zip", "Neoblox.zip");
+                        Thread.Sleep(2000);
+                        Console.WriteLine("Successfully downloaded Neoblox!");
+                        normalConsole();
+
+                        space();
+                    }
+                }
+                catch
+                {
+                    space();
+
+                    redConsole();
+                    Console.WriteLine("Failed to download Neoblox!");
+
+                    Thread.Sleep(2000);
+
+                    System.Environment.Exit(1);
+                }
+            }
+
+            try
+            {
+                greenConsole();
+                Console.WriteLine("Extracting Neoblox.zip....");
+
+                Thread.Sleep(5000);
+
+                string neobloxArchive = $"{AppDomain.CurrentDomain.BaseDirectory}\\Neoblox.zip";
+
+                ZipFile.ExtractToDirectory(neobloxArchive, AppDomain.CurrentDomain.BaseDirectory);
+
+                File.Delete($"{AppDomain.CurrentDomain.BaseDirectory}\\Neoblox.zip");
+
+                space();
+
+                Console.WriteLine("Successfully extracted Neoblox.zip!");
+
+                Thread.Sleep(2000);
+
+                System.Diagnostics.Process.Start($"{AppDomain.CurrentDomain.BaseDirectory}\\Neoblox");
+            }
+
+            catch
+            {
+                space();
+
+                redConsole();
+                Console.WriteLine("Failed to extract Neoblox!");
+
+                Thread.Sleep(2000);
+
+                System.Environment.Exit(1);
+            }
         }
     }
 }
